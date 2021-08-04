5bc22fea182caafb869cbf57622685faac11839b
Martena Yousef <martenayosef865@gmail.com>
2021-08-04 03:19:14 -0700

    simple change Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switchStopWach();
+
+        }
+
+         static void switchStopWach()
+        {
+            var stopwatch = new StopWatch();
+            while (true)
+            {
+                Console.Write("Enter b for start, or s for stop, or any other charater to exsit: ");
+                var input = Console.ReadLine();
+
+                if (input == "b")
+                {
+                    stopwatch.Start();
+                }
+                else if (input == "s")
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine("duration is " + stopwatch.GetDuration());
+                }
+                else
+                {
+                    Console.WriteLine("duration is " + stopwatch.GetDuration());
+                    break;
+                }
+            }
+        }
+    }
+}
