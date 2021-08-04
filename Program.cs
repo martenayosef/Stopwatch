a4d807b806440d4e5298049da467964b0de2d6ec
Martena Yousef <martenayosef865@gmail.com>
2021-08-04 03:19:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var stopwatch = new StopWatch();
+            while (true)
+            {
+                Console.Write("Enter b for start, or s for stop, or any other charater to exsit: ");
+                var input = Console.ReadLine();
+           
+                if(input == "b")
+                {
+                    stopwatch.Start();
+                } else if(input == "s")
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine("duration is " + stopwatch.GetDuration());
+                }
+                else
+                {
+                    Console.WriteLine("duration is " + stopwatch.GetDuration());
+                    break;
+                }             
+            }            
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isRunning;
+         
+        public void Start ()
+        {
+            if (!_isRunning)
+            {
+                _startTime = DateTime.Now;
+                _isRunning = true;
+            } 
+        }
+
+        public void Stop ()
+        {
+            if (_isRunning)
+            {
+                _endTime = DateTime.Now;
+                _isRunning = false;
+            }
+        }
+        public TimeSpan GetDuration ()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
